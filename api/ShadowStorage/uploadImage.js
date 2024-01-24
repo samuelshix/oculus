59d067ba1217eac1449fac3bc71301a8e15eefd7
samshi <samshi@iu.edu>
2024-01-24 10:27:00 -0500

    log the key

--- api/ShadowStorage/uploadImage.js
+++ api/ShadowStorage/uploadImage.js
@@ -8,20 +8,19 @@ dotenv.config();
 
 export async function uploadImage(fileBuffer) {
     let key = process.env.SHADOW_SECRET_KEY
+    console.log(key)
     let keypair = Keypair.fromSecretKey(Uint8Array.from(key));
     const connection = new Connection(
         `https://rpc.helius.xyz/?api-key=${process.env.NEXT_PUBLIC_HELIUS_API_KEY}`, "confirmed"
     );
-    console.log(fileBuffer)
     const wallet = new anchor.Wallet(keypair);
     const drive = await new ShdwDrive(connection, wallet).init();
     const accts = await drive.getStorageAccounts("v2");
     let acctPubKey = new anchor.web3.PublicKey(accts[0].publicKey);
     const fileToUpload = {
-        name: "Porftolio1.png",
+        name: "Porftolio.png",
         file: fileBuffer,
     };
-    console.log(fileToUpload)
     const uploadFile = await drive.uploadFile(acctPubKey, fileToUpload);
     console.log(uploadFile);
     return uploadFile.finalized_locations[0];
